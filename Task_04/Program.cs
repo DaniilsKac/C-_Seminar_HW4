1930f81fc4b19aefee84f2c3be947ba310ec1763
Daniils_Kackins <daniil.kackin@gmail.com>
2023-07-04 20:13:15 +0100

    Добавил задачу 29 (ввод с консоли)

---
+++ Task_04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов в диапазоне от 10 до 1000 и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// С КОНСОЛИ
+
+Console.Write("Введите размер массива: ");
+int AlphaSize = Convert.ToInt32(Console.ReadLine());
+int[] Alpha = new int[AlphaSize];
+
+for (int i = 0; i < Alpha.Length; i++)
+{
+    Console.Write($"Введите элемент массива {i}:  ");
+    Alpha[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Массив: [ {string.Join("; " , Alpha)} ]");
